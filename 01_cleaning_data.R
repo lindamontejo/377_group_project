5ca490c1faa2a7c318495c81964ae28ed82e43b2
Linda Montejo <jane@example.com>
2026-03-12 19:47:21 -0500

    turned some character into factors

--- 01_cleaning_data.R
+++ 01_cleaning_data.R
@@ -1,20 +1,22 @@
 
-
-
+## loading packages ---
 library(tidyverse)
 library(naniar)
 library(lubridate)
 
 
-sa <- read_csv("sexual_assaults.csv") |> 
+## read in data ----
+# sexual assualt 
+sa <- read_csv("data/sexual_assaults.csv") |> 
   janitor::clean_names()
-
+# sexual offenses 
 so <- read_csv("sex_o.csv") |> 
   janitor::clean_names()
 
+
+## Cleaning data (changing date column) ----
 so <- so |>
   mutate(date = as.POSIXct(date, format = "%m/%d/%Y %H:%M:%S"))
-
 
 so_year <- so |>
   mutate(
@@ -28,16 +30,19 @@ so_year <- so |>
 write.csv(so_year, file = "sex_offenses_month.csv")
 
 
+is.factor(so$description)
+levels (so$description)
+cat_vars <- names(so)[sapply(so, function(x) is.factor(x) | is.character(x))]
 
 
-
-is.factor(sa$description)
-
-is.factor(sa$description)
-
-
-sa_fct <- sa |> 
+so <- so |> 
+  mutate(across(all_of(cat_vars), ~ tolower(.))) |> 
   mutate(
     description = factor(description), 
-    location_description = factor(location_description)
+    location_description = factor(location_description), 
+    arrest = factor(arrest), 
+    domestic = factor(domestic)
   )
+
+write.csv(so, file = "data/sex_offenses_clean.csv")
+
